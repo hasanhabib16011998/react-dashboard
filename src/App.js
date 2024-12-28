1facf412f7995567ffd42227236eced54ef6f837
hasanhabib16011998 <hasan16habib@gmail.com>
2024-12-29 01:08:29 +0600

    completed sidebar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import DashBoard from './pages/dashboard';
 import Header from './components/Header';
+import SideBar from './components/Sidebar';
 
 function App() {
   return (
     <BrowserRouter>
     <Header/>
+    <div className='main d-flex'>
+      <div className='sidebarWrapper'>
+        <SideBar/>
+      </div>
+      <div className='content'>
       <Routes>
         <Route path={'/'} exact={true} element={<DashBoard/>}/>
         <Route path={'/dashboard'} exact={true} element={<DashBoard/>}/>
       </Routes>
+
+      </div>
+
+    </div>
+
     </BrowserRouter>
   );
 }
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,141 @@
+import React,{useState} from 'react';
+import "./sidebar.css";
+import { LuLayoutDashboard } from "react-icons/lu";
+import { FaAngleRight,FaAngleDown } from "react-icons/fa";
+import { AiOutlineProduct } from "react-icons/ai";
+import { FaCartArrowDown } from "react-icons/fa";
+import { LuMessageSquareText } from "react-icons/lu";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { IoIosSettings } from "react-icons/io";
+import { Link } from 'react-router-dom';
+
+export default function SideBar() {
+    const [isProductMenuOpen, setIsProductMenuOpen] = useState(false);
+    const toggleProductMenu = () => {
+        setIsProductMenuOpen(!isProductMenuOpen);
+    };
+  return (
+    <>
+    <div className='sidebar'>
+        <ul>
+            <li>
+                <button className='w-100'>
+                    <span className='icon'><LuLayoutDashboard/></span>
+                    DashBoard
+                    <span className='arrow'><FaAngleRight/></span>
+                </button>
+            </li>
+            <li>
+                <button className='w-100' onClick={toggleProductMenu}>
+                    <span className='icon'><AiOutlineProduct/></span>
+                    Products
+                    <span className='arrow'>
+                    {isProductMenuOpen ? <FaAngleDown /> : <FaAngleRight />}
+                    </span>
+                </button>
+                {isProductMenuOpen && (
+                <ul className='submenu'>
+                    <li><Link to="#">Product List</Link></li>
+                    <li><Link to="#">Product View</Link></li>
+                    <li><Link to="#">Product Upload</Link></li>
+                </ul>
+                 )}
+            </li>
+            <li>
+                <button className='w-100'>
+                    <span className='icon'><FaCartArrowDown/></span>
+                    Orders
+                    <span className='arrow'><FaAngleRight/></span>
+                </button>
+            </li>
+            <li>
+                <button className='w-100'>
+                    <span className='icon'><LuMessageSquareText/></span>
+                    Messages
+                    <span className='arrow'><FaAngleRight/></span>
+                </button>
+            </li>
+            <li>
+                <button className='w-100'>
+                    <span className='icon'><IoIosNotificationsOutline/></span>
+                    Notifications
+                    <span className='arrow'><FaAngleRight/></span>
+                </button>
+            </li>
+            <li>
+                <button className='w-100'>
+                    <span className='icon'><IoIosSettings/></span>
+                    Settings
+                    <span className='arrow'><FaAngleRight/></span>
+                </button>
+            </li>
+            <li>
+                <button className='w-100'>
+                    <span className='icon'><LuLayoutDashboard/></span>
+                    Products
+                    <span className='arrow'><FaAngleRight/></span>
+                </button>
+            </li>
+            <li>
+                <button className='w-100'>
+                    <span className='icon'><LuLayoutDashboard/></span>
+                    Products
+                    <span className='arrow'><FaAngleRight/></span>
+                </button>
+            </li>
+            <li>
+                <button className='w-100'>
+                    <span className='icon'><LuLayoutDashboard/></span>
+                    Products
+                    <span className='arrow'><FaAngleRight/></span>
+                </button>
+            </li>
+            <li>
+                <button className='w-100'>
+                    <span className='icon'><LuLayoutDashboard/></span>
+                    Products
+                    <span className='arrow'><FaAngleRight/></span>
+                </button>
+            </li>
+            <li>
+                <button className='w-100'>
+                    <span className='icon'><LuLayoutDashboard/></span>
+                    Products
+                    <span className='arrow'><FaAngleRight/></span>
+                </button>
+            </li>
+            <li>
+                <button className='w-100'>
+                    <span className='icon'><LuLayoutDashboard/></span>
+                    Products
+                    <span className='arrow'><FaAngleRight/></span>
+                </button>
+            </li>
+            <li>
+                <button className='w-100'>
+                    <span className='icon'><LuLayoutDashboard/></span>
+                    Products
+                    <span className='arrow'><FaAngleRight/></span>
+                </button>
+            </li>
+            <li>
+                <button className='w-100'>
+                    <span className='icon'><LuLayoutDashboard/></span>
+                    Products
+                    <span className='arrow'><FaAngleRight/></span>
+                </button>
+            </li>
+            <li>
+                <button className='w-100'>
+                    <span className='icon'><LuLayoutDashboard/></span>
+                    Products
+                    <span className='arrow'><FaAngleRight/></span>
+                </button>
+            </li>
+            
+        </ul>
+
+    </div>
+    </>
+  )
+}
